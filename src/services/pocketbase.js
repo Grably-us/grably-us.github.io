d68012cf442fac15090362a78860c020aa45873d
Anton Vice <C-Anton.Vice@UDX.com>
2024-08-01 14:04:36 -0400

    t

--- src/services/pocketbase.js
+++ src/services/pocketbase.js
@@ -1,3 +1,3 @@
 import PocketBase from 'pocketbase';
 
-export const pb = new PocketBase('https://db-long-frog-5929.fly.dev');
+export const pb = new PocketBase('https://db-long-frog-5929.fly.dev/api/');
